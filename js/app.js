cd6036b7705f2ef980315fe6ca2ceb83954058bb
Haitham AbdAllah <haithamabdallah@gmail.com>
2024-08-19 20:40:09 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+var swiper = new Swiper(".intro", {
+    direction: "vertical",
+    cssMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
